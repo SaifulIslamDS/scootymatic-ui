6bf01479c089701dda547e83f8b90207329a4542
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-11-10 19:04:34 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -9,19 +9,30 @@ import Home from './Pages/Home/Home/Home';
 import Header from './Shared/Header/Header';
 import Explore from './Pages/Explore/Explore/Explore';
 import Footer from './Shared/Footer/Footer';
+import Dashboard from './Admin/Dashboard/Dashboard';
+import Login from './Admin/Login/Login';
+import Register from './Admin/Register/Register';
 
 function App() {
   return (
     <div id="main">
       <Router>
         <Header></Header>
-
         <Switch>
           <Route exact path="/">
             <Home></Home>
           </Route>
           <Route path="/explore">
             <Explore></Explore>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard ></Dashboard>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
           </Route>
         </Switch>
 
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
-    );
-};
+        <header id="header">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <h1>Logo</h1>
+                    </div>
+                    <div className="col-md-8">
+                        <Link to="/">Home</Link>
+                        <Link to="/explore">Explore</Link>
+                        <Link to="/dashboard">Dashboard</Link>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );    
+}
 
 export default Header;
