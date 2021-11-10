233be038d638d047bc9a0fb2365b6ce4ad5f0a4f
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-11-10 13:51:35 +0600

    Update

---
+++ src/Auth/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBGoJ9Ceo2VwC4m1uPpbt9FrH65ZPyEmtY",
+    authDomain: "crimsons-it.firebaseapp.com",
+    projectId: "crimsons-it",
+    storageBucket: "crimsons-it.appspot.com",
+    messagingSenderId: "763860173133",
+    appId: "1:763860173133:web:0d5e8dc93c34b1d2fe7704"
+};
+export default firebaseConfig;
