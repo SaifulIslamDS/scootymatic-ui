0c59287b55846cd652d659ad395a57b4ec40389b
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-11-14 18:47:15 +0600

    Update

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Banner.css';
+import bannerImage from '../../../Images/banner-image.jpg';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div>
-            <h1 className="display-1">This is banner</h1>
-            
-        </div>
+        <section id="banner" style={{ backgroundImage:`url(${bannerImage})` }}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1 className="banner-title">Let's get lost with <br />
+                            <span>HONDA SH350I</span>
+                        </h1>
+                        <Link to="/explore"><button className="btn btn-outline-light">
+                            Explore More
+                        </button></Link>
+                    </div>
+                </div>
+            </div>            
+        </section>
     );
 };
 
