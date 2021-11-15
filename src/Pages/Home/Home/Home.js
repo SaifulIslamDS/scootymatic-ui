173bf41ff7fcdfc7320ffd3e687a8afe6ee8671f
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-11-15 08:41:24 +0600

    Update

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './Home.css';
-import Header from '../../../Shared/Header/Header';
-import Footer from '../../../Shared/Footer/Footer';
 import Banner from '../Banner/Banner';
 import HomeProducts from '../HomeProducts/HomeProducts';
 import Reviews from '../Reviews/Reviews';
